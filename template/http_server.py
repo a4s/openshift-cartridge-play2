0af64b441789d3e1f5406960c8659635dadaa234
Amir Kadić <amir.kadic@gmail.com>
2015-10-26 06:03:37 +0100

    Debug: simple http server

--- template/http_server.py
+++ template/http_server.py
@@ -1,14 +1,17 @@
 import SimpleHTTPServer
 import SocketServer
+import os
+import socket
 
 # minimal web server.  serves files relative to the
 # current directory.
 
 PORT = 8080
+local_hostname = os.getenv("OPENSHIFT_INTERNAL_IP")
 
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 
-httpd = SocketServer.TCPServer(("", PORT), Handler)
+httpd = SocketServer.TCPServer((socket.gethostbyname (local_hostname), PORT), Handler)
 
 print "serving at port", PORT
 httpd.serve_forever()
